de147029358fbf9ccd860dcfc2f61c4cd64fbb85
unknown <nicholas_sc@live.com>
2019-09-05 13:40:22 -0300

    rename

---
+++ backend/main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
+
+app.listen(21086, () => {
+    console.log('Backend executando...')
+})
